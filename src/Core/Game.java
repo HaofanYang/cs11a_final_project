2fd20e145506c8a11266fd239339f765b6c9adb3
haofan yang <haofanyang@brandeis.edu>
2018-11-27 19:01:49 -0500

    Implementd promptSeed method

--- src/Core/Game.java
+++ src/Core/Game.java
@@ -8,9 +8,21 @@ import java.awt.*;
 public class Game {
     private static final int width = 71;
     private static final int height = 41;
-    private boolean hasFinished;
+    private boolean finished;
     private int round;
     private Random rand;
+    private int X;
+    private int Y;
+    private RandomMaze MAZE;
+
+    public Game(RandomMaze maze, int x, int y, int round, Random rand, boolean finished) {
+        this.MAZE = maze;
+        this.X = x;
+        this.Y = y;
+        this.round = round;
+        this.rand = rand;
+        this.finished = finished;
+    }
 
     public static void main(String[] args) {
         showMenu();
@@ -18,6 +30,7 @@ public class Game {
 
     private static void showMenu() {
         StdDraw.setCanvasSize(1024, 512);
+        StdDraw.enableDoubleBuffering();
         StdDraw.clear(Color.BLACK);
         StdDraw.setPenColor(Color.WHITE);
         StdDraw.setYscale(0, height);
@@ -45,13 +58,41 @@ public class Game {
         }
     }
 
+
+    private static long promptSeed() {
+        StringBuilder seed = new StringBuilder();
+        while (true) {
+            StdDraw.clear(Color.BLACK);
+            StdDraw.setFont(new Font("Arial", Font.BOLD, 50));
+            StdDraw.text(width / 2, 2 * height / 3, "Please provide a seed number");
+            StdDraw.setFont(new Font("Arial", Font.BOLD, 20));
+            StdDraw.text(width / 2, height / 2 - height / 10, "hit S to submit and D to delete");
+            StdDraw.setFont(new Font("Arial", Font.ITALIC, 40));
+            StdDraw.text(width / 2, height / 2 - height / 5, seed.toString() + "|");
+            StdDraw.show();
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                if (key - '0' >= 0 && key - '0' <= 9) {
+                    seed.append(key);
+                } else if (seed.length() > 0 && key == 'S' || key == 's' ) {
+                    break;
+                } else if (seed.length() > 0 && key == 'd' || key == 'D') {
+                    seed.deleteCharAt(seed.length() - 1);
+                }
+            }
+        }
+        return Long.parseLong(seed.toString());
+    }
     //TODO
     private static void startNewGame() {
+        long seed = promptSeed();
+        /***
         StdDraw.clear(Color.BLACK);
         TETile[][] world = new RandomMaze(width, height).getWorld();
         TERenderer rend = new TERenderer();
         rend.initialize(width, height);
         rend.renderFrame(world);
+         **/
     }
 
     //TODO
